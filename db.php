6ae3c3a62204c9daa3c5eef008fead696c34cee9
s-avdonin <serhii.avdonin@gmail.com>
2019-01-21 20:06:32 +0200

    db.php updated to create DB

--- db.php
+++ db.php
@@ -1,4 +1,26 @@
 <?php
+// если нужно, создаём базу данных и таблицы
+CREATE DATABASE IF NOT EXISTS keepnotes CHARACTER SET latin1 COLLATE latin1_swedish_ci
+
+USE keepnotes;
+CREATE  TABLE IF NOT EXISTS keepnotes.user (
+  id INT  AUTO_INCREMENT ,
+  name VARCHAR(30),
+  login VARCHAR(30) ,
+  pass VARCHAR(64),
+   PRIMARY KEY (id) )
+ENGINE = InnoDB;
+
+CREATE  TABLE IF NOT EXISTS keepnotes.notes (
+  id INT  AUTO_INCREMENT ,
+  title TINYTEXT,
+  text TEXT ,
+  last_change DATETIME,
+  users_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES user(id)  )
+ENGINE = InnoDB;
+
 // создаём переменную в которую записываем подключение к БД
 $connection = mysqli_connect ("localhost", "db_user", "1234", "keepnotes");
 // устанавливаем для подключения кодировку utf8
@@ -6,6 +28,11 @@ mysqli_set_charset($connection,"utf8" );
 // если подключение неудачно выводим ошибку
 if(!$connection)
 {
+	 
+            <script>
+                // сообщение об ошибке подключения
+                alert("Не удалось подключиться к базе данных.");
+             </script>
     mysqli_error($connection);
 }
 ?>
