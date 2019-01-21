9d8236cc512f929edd0723db767ab88fa58bc6e6
s-avdonin <serhii.avdonin@gmail.com>
2019-01-21 21:09:17 +0200

    db.php updated 6

--- db.php
+++ db.php
@@ -9,28 +9,6 @@ mysqli_set_charset($connection,"utf8" );
 // если подключение неудачно выводим ошибку
 if(!$connection)
 {
-	mysqli_query($connection, "CREATE DATABASE IF NOT EXISTS keepnotes CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci
-
-USE keepnotes;
-CREATE  TABLE IF NOT EXISTS keepnotes.user (
-  id INT  AUTO_INCREMENT ,
-  name VARCHAR(30),
-  login VARCHAR(30) ,
-  pass VARCHAR(64),
-   PRIMARY KEY (id) )
-ENGINE = InnoDB;
-
-CREATE  TABLE IF NOT EXISTS keepnotes.notes (
-  id INT  AUTO_INCREMENT ,
-  title TINYTEXT,
-  text TEXT ,
-  last_change DATETIME,
-  users_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (users_id) REFERENCES user(id)  )
-ENGINE = InnoDB;");
-  	    mysqli_close($connection);  
-		mysqli_error($connection);
-
+	mysqli_error($connection);
 }
 ?>
---
+++ keepnotes.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 21 2019 г., 20:07
+-- Версия сервера: 10.1.37-MariaDB
+-- Версия PHP: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `keepnotes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `title` tinytext,
+  `text` text,
+  `last_change` datetime DEFAULT NULL,
+  `users_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `login` varchar(30) DEFAULT NULL,
+  `pass` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
