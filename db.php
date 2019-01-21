822fea037d2d28e925803b5e76ab10b782600f1d
s-avdonin <serhii.avdonin@gmail.com>
2019-01-22 00:29:21 +0200

    updated 10

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 
 // создаём переменную в которую записываем подключение к БД
 //$connection = mysqli_connect ("localhost", "db_user", "1234", "keepnotes");
-$connection = mysqli_connect ("eu-cdbr-west-02.cleardb.net", "b003195f256764", "4fa37762", "heroku_420524c624766a5");
+$connection = mysqli_connect ("eu-cdbr-west-02.cleardb.net:3306", "b003195f256764", "4fa37762", "heroku_420524c624766a5");
 echo("Connection success")
 // устанавливаем для подключения кодировку utf8
 // mysqli_set_charset($connection,"utf8" );
